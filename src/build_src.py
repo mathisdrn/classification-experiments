4bf803001f1863ee3f40d5ea629c4f327fe183cf
MathisDrn <116900975+mathisdrn@users.noreply.github.com>
2025-03-01 21:27:06 +0100

    feat: added implementation

--- src/build_src.py
+++ src/build_src.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 from get_dataset import dataset_loaders
-# Parametrize and execute notebooks to build source code
 from papermill import execute_notebook
+import nbformat as nbf
 
 DESTINATION_FOLDER = Path("src_built/")
 print(f"Working directory: {Path.cwd()}")
@@ -16,8 +16,20 @@ def build_src():
         
     for i, dataset in enumerate(datasets):
         output_path = DESTINATION_FOLDER / f"{i} - {dataset}.ipynb"
-        execute_notebook(TEMPLATE, output_path, parameters={"dataset": dataset})
-    pass
+        execute_notebook(TEMPLATE, output_path, parameters={"dataset": dataset}, prepare_only=True)
+        
+        # Load the notebook
+        nb = nbf.read(output_path, as_version=4)
+        
+        # Create a new markdown cell with the frontmatter
+        frontmatter = f"---\ntitle: {dataset}\n---"
+        md_cell = nbf.v4.new_markdown_cell(frontmatter)
+        
+        # Insert the markdown cell at the top of the notebook
+        nb.cells.insert(0, md_cell)
+        
+        # Write the notebook back to the file
+        nbf.write(nb, output_path)
 
 if __name__ == "__main__":
     build_src()
